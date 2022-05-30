be3fbf06abd150fc6c3dc7ee504b907ec1414bc4
lGrossiC <lucasgrossicostick@gmail.com>
2022-05-29 23:46:35 -0300

    Update: main.cpp
    -Removed: tecla "X" para fechar o jogo;
    -Update: indentação.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #define esc 27
 
 using namespace std;
+
 bool gameOver;
 const int altura = 20;
 const int largura = 20;
 int x, y, frutaX, frutaY, pontos, escolha;
 
-/* Eu quero que a cobra se mova CONSTANTEMENTE para a posição indicada pelo jogador ao invés de fazer o usário pressionar varias vezes a DIRECAO que ele   
-quer movimentar a cobra. Essas direções porém, são fixas: Para a equerda,para a direita, para cima e para baixo. A unica forma que encontrei na internet 
+/* Eu quero que a cobra se mova CONSTANTEMENTE para a posição indicada pelo jogador ao invés de fazer o usário pressionar varias vezes a DIRECAO que ele
+quer movimentar a cobra. Essas direções porém, são fixas: Para a equerda,para a direita, para cima e para baixo. A unica forma que encontrei na internet
 de fazer isso é utilizando enumeração */
 enum direcao
 {
@@ -44,10 +45,9 @@ void Grafico()
     /* é necessário utilizar o comando "cls" dentro do console para não imprimir um mapa atrás do outro */
     system("cls");
 
-
     /* loop "for" para borda de cima do mapa*/
     for (int l = 0; l < largura; l++)
-    cout << "#";
+        cout << "#";
     cout << endl;
 
     /* loop "for" para bordas laterais do mapa*/
@@ -57,17 +57,17 @@ void Grafico()
         {
             if (l == 0)
                 cout << "#";
-            
+
             /* se a posição for igual a posição da cabeça da cobra: cout cabeça da cobra "O" */
             if (h == y && l == x)
                 cout << "O";
-            
+
             /* se a posição for igual a posição da fruta: cout fruta "F" */
             else if (h == frutaY && l == frutaX)
                 cout << "F";
-            
+
             /* se a posição =! (x,y) && (frutaX, frutaY): cout espaço em branco " " */
-            else    
+            else
                 cout << " ";
 
             if (l == largura - 1)
@@ -79,7 +79,7 @@ void Grafico()
     /* loop "for" para borda de baixo do mapa*/
     for (int l = 0; l < largura; l++)
         cout << "#";
-        cout << endl;
+    cout << endl;
 }
 
 void Usabilidade()
@@ -87,16 +87,16 @@ void Usabilidade()
     /* verifica se alguma tecla foi pressionada e retorna 1 caso sim e 0 caso não.*/
     if (kbhit())
     {
-        /* 
-        
+        /*
+
         retorna o valor 1 para tecla pressionada
-        
+
         switch para as seguintes teclas:
         w -> cima
         a -> esquerda
         s -> baixo
-        d -> direita 
-         
+        d -> direita
+
         */
         switch (_getch())
         {
@@ -108,7 +108,7 @@ void Usabilidade()
         case 'd':
             dir = direita;
             break;
-        /* define o valor da enum "dir" para "cima"*/    
+        /* define o valor da enum "dir" para "cima"*/
         case 'w':
             dir = cima;
             break;
@@ -117,9 +117,6 @@ void Usabilidade()
             dir = baixo;
             break;
         /* define o valor de gameOver para true para finalizar o jogo*/
-        case 'x':
-            gameOver = true;
-            break;
         case esc:
             gameOver = true;
             break;
@@ -127,59 +124,55 @@ void Usabilidade()
     }
 }
 void Logica()
-{   
-    /* switch na "enum dir" para que, por exemplo, se o valor atribuído a a "enum dir" for "esquerda" o valor x (que define a posição da cabeça da cobra) 
+{
+    /* switch na "enum dir" para que, por exemplo, se o valor atribuído a "enum dir" for "esquerda" o valor x (que define a posição da cabeça da cobra)
     vai receber x-1, ou seja vai movimentar uma casa para a esquerda e assim por diante... */
     switch (dir)
     {
-       case esquerda:
-       x--;
-       break; 
+    case esquerda:
+        x--;
+        break;
 
-       case direita:
-       x++;
-       break; 
+    case direita:
+        x++;
+        break;
 
-       case cima:
-       y--;
-       break; 
+    case cima:
+        y--;
+        break;
 
-       case baixo:
-       y++;
-       break; 
+    case baixo:
+        y++;
+        break;
     }
-
 }
 int main()
 {
 
-
-    /* menu que permite o jogador "fechar" ou "jogar" o jogo. esse menu precisa aparecer sempre que o jogo finalizar, 
+    /* menu que permite o jogador "fechar" ou "jogar" o jogo. esse menu precisa aparecer sempre que o jogo finalizar,
     permitindo que o usuário jogue novamente sem ter que compilar o código múltiplas vezes. */
-    
-    
+
     cout << "1 - Jogar\n";
     cout << "2 - Ranking\n";
-    cout << "0 - Fechar jogo\n"; 
-    
-    
+    cout << "0 - Fechar jogo\n";
+
     cin >> escolha;
 
-    switch(escolha)
+    switch (escolha)
     {
-        case 0: 
-            cout << "Obrigado por jogar :)";
-            return 0;
-        case 1:
-            cout << "teste";
-            break;
-        case 2:
-            cout << "RANKING";
-            return 0;
+    case 0:
+        cout << "Obrigado por jogar :)";
+        return 0;
+    case 1:
+        cout << "teste";
+        break;
+    case 2:
+        cout << "RANKING";
+        return 0;
     }
 
     Configuracao();
-    
+
     /* estrutura condicional "enquanto" para que enquanto o jogo não esteja em "gameOver" a função Configuração chame as outras funções necessárias */
     while (!gameOver)
     {
